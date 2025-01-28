4e905e26862c47df748163c6a2fc2f329b6cab10
Gauge Garcia <gaugegarcia@my.whccd.edu>
2025-01-27 18:34:01 -0800

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Current Hour Prompt
+            Console.Write("Enter the current hour (0-23);");
+            int hour= int.Parse(Console.ReadLine());
+
+
+            if (hour >= 5 && hour <= 11)
+            {
+                Console.WriteLine("Good Morning!");
+            }
+            else if (hour >= 12 && hour <= 17)
+            {
+                Console.WriteLine("Good Afternoon!");
+            }
+            else if(hour >= 18 && hour <=20)
+            {
+                Console.WriteLine("Good Evening!");
+            }
+            else
+            {
+                Console.WriteLine("Good Night!");
+            }
+        }
+    }
+}
